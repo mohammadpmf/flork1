e71d2e5372155baf0c1bf801dd4abe160cef6052
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2023-10-08 16:14:07 +0330

    After 1

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class BPost(models.Model):
+    STATUS_CHOICES = (
+        ('p', 'published'),
+        ('d', 'draft'),
+    )
+    title = models.CharField(max_length=60)
+    text = models.TextField()
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)

8d5cd65ae1506257bc146f43dc66e4c512139404
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2023-10-15 16:10:52 +0330

    Session 3

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import BPost
 
-# Create your views here.
+def posts_list(request):
+    posts = BPost.objects.all()
+    context = {'postha': posts}
+    return render(request, 'posts_list.html', context)

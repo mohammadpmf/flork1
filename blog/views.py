21ae55d36353c679924d408eb8c505d2873585d0
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2023-10-31 16:07:48 +0330

    Sesssion 8

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,24 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
 from .models import BPost
+from .forms import NewPostForm
 
 def posts_list(request):
     # posts = BPost.objects.all()
-    posts = BPost.objects.filter(status='p')
+    posts = BPost.objects.filter(status='p').order_by('-datetime_modified')
     context = {'postha': posts}
     return render(request, 'posts_list.html', context)
 
 def show_detail(request, pk):
     post = get_object_or_404(BPost, pk=pk)
     return render(request, 'detail.html', {'post': post})
-from .forms import NewPostForm
+
 def new_post(request):
     form = NewPostForm()
+    if request.method=='POST':
+        form = NewPostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form = NewPostForm()
+            return redirect(reverse("blog"))
     return render(request, 'new_post.html', {'form': form})

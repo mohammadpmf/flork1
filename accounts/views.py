57c9fa021b7ed2fb3c2caf129e6dbc5920c2a4ed
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2023-12-03 16:19:40 +0330

    End of weblog

--- accounts/views.py
+++ accounts/views.py
@@ -2,20 +2,20 @@ from django.shortcuts import render, redirect
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from . import models
+# from . import models
 
-class SignUpView(generic.CreateView):
-    model = UserCreationForm
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
     template_name = 'registration/signup.html'
     success_url = reverse_lazy('login')
     
-def signup(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password1 = request.POST.get('password1')
-        password2 = request.POST.get('password2')
-        if password1!=password2:
-            return render(request, 'registration/signup.html', {"error": "password1 and password2 does not match"})
-        models.Account.objects.create(username=username, password=password1)
-        return redirect('login')
-    return render(request, 'registration/signup.html')
+# def signup(request):
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password1 = request.POST.get('password1')
+#         password2 = request.POST.get('password2')
+#         if password1!=password2:
+#             return render(request, 'registration/signup.html', {"error": "password1 and password2 does not match"})
+#         models.Account.objects.create(username=username, password=password1)
+#         return redirect('login')
+#     return render(request, 'registration/signup.html')
